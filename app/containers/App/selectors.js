c718fdf8cc892afe927905d5ac9578a002f2404e
EvaFro <frolli.erin@gmail.com>
2018-05-07 15:45:22 -0700

    refactor to include loding data selectors

--- app/containers/App/selectors.js
+++ app/containers/App/selectors.js
@@ -1,12 +1,44 @@
 import { createSelector } from 'reselect';
 
+const selectGlobal = (state) => state.get('global');
 const selectRoute = (state) => state.get('route');
 
+// Location
 const makeSelectLocation = () => createSelector(
   selectRoute,
   (routeState) => routeState.get('location').toJS()
 );
 
+// Loading Data
+const makeSelectLoading = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('loading')
+);
+
+// CurrentUser Success
+const makeSelectCurrentUser = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.getIn(['postData', 'currentUser'])
+);
+
+// Messages Success
+const makeSelectMessages = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.getIn(['postData', 'savedMessages'])
+);
+
+// Error
+const makeSelectError = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('error')
+);
+
+
 export {
+  selectGlobal,
   makeSelectLocation,
+  makeSelectLoading,
+  makeSelectCurrentUser,
+  makeSelectMessages,
+  makeSelectError,
 };
