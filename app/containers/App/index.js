9a98053df304f8778f3f8a9a47fa342fc5da3155
EvaFro <frolli.erin@gmail.com>
2018-05-07 14:25:07 -0700

    refactor 2 include MessageBoardPage & Header

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,17 +13,31 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import HomePage from 'containers/HomePage/Loadable';
+import MessageBoardPage from 'containers/MessageBoardPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'components/Header';
+
+const AppWrapper = styled.div`
+  max-width: calc(768px + 16px * 2);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 16px;
+  flex-direction: column;
+`;
 
 export default function App() {
   return (
-    <div>
+    <AppWrapper>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/messageboard" component={MessageBoardPage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </div>
+    </AppWrapper>
   );
 }
