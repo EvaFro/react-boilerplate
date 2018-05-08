a26aa0664301892be571c82f94a1cb94800e000e
EvaFro <frolli.erin@gmail.com>
2018-05-07 20:42:58 -0700

    add DEFAULT_LOCALE back

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -12,3 +12,4 @@
 export const LOAD_MESSAGES = 'boilerplate/App/LOAD_MESSAGES';
 export const LOAD_MESSAGES_SUCCESS = 'boilerplate/App/LOAD_MESSAGES_SUCCESS';
 export const LOAD_MESSAGES_ERROR = 'boilerplate/App/LOAD_MESSAGES_ERROR';
+export const DEFAULT_LOCALE = 'en';
