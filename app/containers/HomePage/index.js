54bd954da1c7a03509e859a483c6820f58d61840
EvaFro <frolli.erin@gmail.com>
2018-05-08 08:13:19 -0700

    add textrea form to HomePage component

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,39 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+// import reducer from './reducer';
+
+import Form from './Form';
+import TextArea from './TextArea';
+import Button from './Button';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <h3>
+          <FormattedMessage {...messages.instructions} />
+        </h3>
+        <Form>
+          <TextArea
+            id="post"
+            type="text"
+            placeholder="Textbox..."
+            value={this.props.post}
+            onChange={this.props.onChangePost}
+          />
+          <Button onClick={this.onSubmitForm()}>Post</Button>
+        </Form>
+      </div>
     );
   }
 }
+
+HomePage.propTypes = {
+  // onSubmitForm: PropTypes.func,
+  post: PropTypes.string,
+  onChangePost: PropTypes.func,
+};
