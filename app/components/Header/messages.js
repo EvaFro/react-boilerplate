4b8d483db99c083ec2cb0fe5ea0afc39923c0aec
EvaFro <frolli.erin@gmail.com>
2018-05-07 17:37:17 -0700

    add page text in a messages file

---
+++ app/components/Header/messages.js
@@ -0,0 +1,21 @@
+/*
+ * HomePage Messages
+ *
+ * This contains all the text for the HomePage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  home: {
+    id: 'boilerplate.components.Header.home',
+    defaultMessage: 'Home',
+  },
+  messages: {
+    id: 'boilerplate.components.Header.messages',
+    defaultMessage: 'Messages',
+  },
+  title: {
+    id: 'boilerplate.components.Header.title',
+    defaultMessage: 'Message Board',
+  },
+});
