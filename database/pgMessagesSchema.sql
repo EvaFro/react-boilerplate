c5419ff8ba19a8b04084a20aa1d8d505eada874e
EvaFro <frolli.erin@gmail.com>
2018-05-08 10:46:43 -0700

    delete username column

--- database/pgMessagesSchema.sql
+++ database/pgMessagesSchema.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS messages;
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY,
-  username text,
   messageText text,
   created_at timestamp without time zone default (now() at time zone 'utc')
 );
