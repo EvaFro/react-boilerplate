193edea7fe919339d841d1ab37c91e8993730d45
EvaFro <frolli.erin@gmail.com>
2018-05-08 20:52:04 -0700

    refactor to take in loading, error and posts

--- app/components/MessageList/index.js
+++ app/components/MessageList/index.js
@@ -5,14 +5,18 @@ import UnsortedList from './UnsortedList';
 import Wrapper from './Wrapper';
 import MessageItem from '../MessageItem';
 
-function MessageList(props) {
+function MessageList({ loading, error, posts }) {
   let content = (<div></div>);
 
   // If we have posts, render them
-  if (props.posts) {
-    content = props.posts.map((item) => (
+  if (posts.length > 0) {
+    content = posts.map((item) => (
       <MessageItem key={`post-item-${item.id}`} messageItem={item} />
     ));
+  } else if (loading) {
+    content = (<MessageItem messageItem={'Loading. Please wait...'} />);
+  } else if (error !== false) {
+    content = (<MessageItem messageItem={'Something went wrong, please try again!'} />);
   } else {
     // Otherwise render a single component
     content = (<MessageItem messageItem={'Sorry, No messages posted yet.'} />);
@@ -28,6 +32,8 @@ function MessageList(props) {
 }
 
 MessageList.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.any,
   posts: PropTypes.array,
 };
 
