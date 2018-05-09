4e48157996d82ec46d70ce2ce3116b0c72cac520
EvaFro <frolli.erin@gmail.com>
2018-05-08 17:08:04 -0700

    start to fill out saga for MessageBoardPage

--- app/containers/MessageBoardPage/saga.js
+++ app/containers/MessageBoardPage/saga.js
@@ -2,33 +2,33 @@
  * Gets the post from Database
  */
 
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
 import { LOAD_MESSAGES } from './constants';
 import { messagesLoaded, messageLoadingError } from './actions';
 
-import request from 'utils/request';
-import { makeSelectMessages } from './selectors';
-
-/**
- * PostgreSQL data request/response handler
- */
-export function* getPosts() {
-  // Select message from store
-  const message = yield select(makeSelectMessages());
-  const requestURL = 'postgres://localhost:5432/message_board';
-
-  try {
-    // Call our request helper (see 'utils/request')
-    const posts = yield call(request, requestURL);
-    yield put(messagesLoaded(posts));
-  } catch (err) {
-    yield put(messageLoadingError(err));
-  }
+// watcher saga: watches for actions dispatched to the store, starts worker saga
+export default function* watcherSaga() {
+  yield takeLatest(LOAD_MESSAGES, workerSaga);
 }
 
-/**
- * Root saga manages watcher lifecycle
- */
-export default function* postData() {
-  yield takeLatest(LOAD_MESSAGES, getPosts);
+function getAllPosts() {
+  return axios({
+    method: 'get',
+    url: 'http://localhost:3000/messages',
+  });
+}
+
+// worker saga: makes the api call when watcher saga sees the action
+function* workerSaga() {
+  try {
+    const response = yield call(getAllPosts);
+    const messages = response.data.message;
+    // dispatch a success action to the store with the messages
+    yield put(messagesLoaded(messages));
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put(messageLoadingError(error));
+  }
 }
