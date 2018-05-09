20717d6df3fcd765cd4c112ce4e014d47cbf1d11
EvaFro <frolli.erin@gmail.com>
2018-05-09 15:00:35 -0700

    flush out HomePage index to have connections

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,15 +12,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-// import reducer from './reducer';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
+
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+import messages from './messages';
 import Form from './Form';
 import TextArea from './TextArea';
 import Button from './Button';
+import { changeCurrentPost, postingMessage } from './actions';
+import { makeSelectCurrentPost, makeSelectPosting, makeSelectError } from './selectors';
+import reducer from './reducer';
+import saga from './saga';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { currentPost, onChangePost, onSubmitForm, error, posting } = this.props;
+
+    console.log('This is currentPost:', currentPost);
+    console.log('This is posting:', posting);
+    console.log('This is error:', error);
+
     return (
       <div>
         <h3>
@@ -28,13 +44,13 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
         </h3>
         <Form>
           <TextArea
-            id="post"
+            id="currentPost"
             type="text"
             placeholder="Textbox..."
-            value={this.props.post}
-            onChange={this.props.onChangePost}
+            value={currentPost}
+            onChange={onChangePost}
           />
-          <Button onClick={this.onSubmitForm()}>Post</Button>
+          <Button onClick={onSubmitForm}>Post</Button>
         </Form>
       </div>
     );
@@ -42,7 +58,38 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
 }
 
 HomePage.propTypes = {
-  // onSubmitForm: PropTypes.func,
-  post: PropTypes.string,
+  currentPost: PropTypes.string,
   onChangePost: PropTypes.func,
+  onSubmitForm: PropTypes.func,
+  posting: PropTypes.bool,
+  error: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
 };
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChangePost: (e) => dispatch(changeCurrentPost(e.target.value)),
+    onSubmitForm: (e) => {
+      if (e !== undefined && e.preventDefault) e.preventDefault();
+      dispatch(postingMessage());
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  currentPost: makeSelectCurrentPost(),
+  posting: makeSelectPosting(),
+  error: makeSelectError(),
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+const withReducer = injectReducer({ key: 'home', reducer });
+const withSaga = injectSaga({ key: 'home', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
