7fc5997e4460029c7e34c92e9502cc8999850734
EvaFro <frolli.erin@gmail.com>
2018-05-09 08:54:07 -0700

    fill out MessageBoardPage to include connections

--- app/containers/MessageBoardPage/index.js
+++ app/containers/MessageBoardPage/index.js
@@ -10,15 +10,79 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
 import MessageList from 'components/MessageList';
+import { makeSelectMessages, makeSelectLoading, makeSelectError } from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import { loadingMessages } from './actions';
 
-export default class MessageBoardPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export class MessageBoardPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  componentDidMount() {
+    if (!this.props.posts) {
+      this.props.onLoadingPage();
+    }
+  }
+
   render() {
+    const { loading, error, posts } = this.props;
+    const postProps = {
+      loading,
+      error,
+      posts,
+    };
+
+    console.log('this is Post:', posts);
+
     return (
       <div>
-        <MessageList />
+        <MessageList {...postProps} />
       </div>
     );
   }
 }
+
+
+MessageBoardPage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+  posts: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.bool,
+  ]),
+  onLoadingPage: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onLoadingPage: (e) => {
+      if (e !== undefined && e.preventDefault) e.preventDefault();
+      dispatch(loadingMessages());
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  posts: makeSelectMessages(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+const withReducer = injectReducer({ key: 'messageBoard', reducer });
+const withSaga = injectSaga({ key: 'messageBoard', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(MessageBoardPage);
